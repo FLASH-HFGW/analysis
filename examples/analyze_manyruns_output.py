b73e8a6a965934373135e45aa3ba07676a3e2eda
GioDho <giorgio.dho@gssi.it>
2026-07-13 16:19:44 +0200

    Addition code to merge all single run analysis results

---
+++ examples/analyze_manyruns_output.py
@@ -0,0 +1,76 @@
+import numpy as np
+import os
+import argparse
+import matplotlib.pyplot as plt
+# matplotlib.use("Agg")
+import gc
+
+
+# ------------------------------------------------------------
+# Parametri da riga di comando
+# ------------------------------------------------------------
+parser = argparse.ArgumentParser(
+    description="Legge un file MIDAS e calcola solo FFT complessa I/Q dei modi TM."
+)
+
+parser.add_argument("--path", required=True,
+                    help="Directory contenente i file npz, es: ~/flash-data/analysis/users/dho/")
+
+parser.add_argument("--out-dir", default="./",
+                    help="Directory dove salvare le FFT")
+args = parser.parse_args()
+
+path = os.path.expanduser(args.path)
+outdir = args.out_dir
+
+run_list=[385,386]
+fft_amp_mode0_SPECs_fin = None
+fft_amp_mode1_SPECs_fin = None
+fft_amp_mode2_SPECs_fin = None
+fft_freq_mode0 = None
+fft_freq_mode1 = None
+fft_freq_mode2 = None
+
+for run in run_list:
+    file_npz = path+"/run%05d.npz"%(run)  
+
+    # Apertura del file
+    data = np.load(file_npz)
+    if fft_amp_mode0_SPECs_fin is None:
+        fft_amp_mode0_SPECs_fin = data["fft_amp_mode0_SPECs"]
+        fft_freq_mode0 = data["fft_freq_mode0"]
+    else:
+        fft_amp_mode0_SPECs_fin += data["fft_amp_mode0_SPECs"]
+        
+    if fft_amp_mode1_SPECs_fin is None:
+        fft_amp_mode1_SPECs_fin = data["fft_amp_mode1_SPECs"]
+        fft_freq_mode1 = data["fft_freq_mode1"]
+    else:
+        fft_amp_mode1_SPECs_fin += data["fft_amp_mode1_SPECs"]
+    
+    if fft_amp_mode2_SPECs_fin is None:
+        fft_amp_mode2_SPECs_fin = data["fft_amp_mode2_SPECs"]
+        fft_freq_mode2 = data["fft_freq_mode0"]
+    else:
+        fft_amp_mode2_SPECs_fin += data["fft_amp_mode2_SPECs"]
+
+run_tot=len(run_list)
+fft_amp_mode0_SPECs_fin/=run_tot
+fft_amp_mode1_SPECs_fin/=run_tot
+fft_amp_mode2_SPECs_fin/=run_tot
+
+out_npz=os.path.join(
+    outdir,
+    "run%05d-%05d_power.npz"
+    % (run_list[0], run_list[-1])
+)
+np.savez_compressed(out_npz,
+    fft_amp_mode0_SPECs=fft_amp_mode0_SPECs_fin,
+    fft_freq_mode0=fft_freq_mode0,
+    fft_amp_mode1_SPECs=fft_amp_mode1_SPECs_fin,
+    fft_freq_mode1=fft_freq_mode1,
+    fft_amp_mode2_SPECs=fft_amp_mode2_SPECs_fin,
+    fft_freq_mode2=fft_freq_mode2,
+)
+
+print("DONE")
